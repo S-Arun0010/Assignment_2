38ba59c93616996a0bfa1cdf7a05ab7dd726bd80
S-Arun0010 <123442099+S-Arun0010@users.noreply.github.com>
2023-02-28 17:17:19 +0530

    Update Clone.java

--- practice/src/main/java/assignmentclone/Clone.java
+++ practice/src/main/java/assignmentclone/Clone.java
@@ -1,61 +1,53 @@
 package assignmentclone;
 import java.util.*;
-import java.util.logging.Logger;
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 public class Clone implements Cloneable {
-	private String x;
-	private String y;
-    public Clone(String x1,String y1)
+	
+	static PrintStream print = new PrintStream(new FileOutputStream(FileDescriptor.out));
+	private int x;
+	private int y;
+    public Clone(int x1,int y1)
     {
     	x=x1;
     	y=y1;
     }
+    
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
 		if (obj == null)
 			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Clone other = (Clone) obj;
-		if (x == null) {
-			if (other.x != null)
-				return false;
-		} else if (!x.equals(other.x))
-			return false;
-		if (y == null) {
-			if (other.y != null)
-				return false;
-		} else if (!y.equals(other.y))
-			return false;
-		return true;
+		return (this.x == ((Clone) obj). x) && (this.y == ((Clone) obj). y); 
 	}
 
-	protected Object clone() throws CloneNotSupportedException
+	public Object clone() throws CloneNotSupportedException
 	{
 		return super.clone();
 	}
+	
 public static void main(String[] args)throws CloneNotSupportedException 
  {   
 	
 	 Scanner sc = new Scanner(System.in);
-	 Logger l= Logger.getLogger("com.api.jar");
-	 l.info("Enter the values of point :");
-	 String x=sc.nextLine();
-	 String y= sc.nextLine();
+	print.println("Enter the values of point :");
+	 int x=sc.nextInt();
+	 int y= sc.nextInt();
 	 Clone p = new Clone(x,y);
-	 l.info("Enter the values of point :");
-	 String a=sc.nextLine();
-	 String b= sc.nextLine();
+	 print.println("Enter the values of point :");
+	 int a=sc.nextInt();
+	 int b= sc.nextInt();
 	 Clone q = new Clone(a,b);
 	 if(p.equals(q))
 		 {
-		 l.info("Both points are equal");
+		 print.println("Both points are equal");
 		 }
 	 else
 	 {
-		 l.info("Both points are not equal");
+		 print.println("Both points are not equal");
 	 }
 	 Clone r = (Clone)p.clone();
-	l.log(Level.INFO,()->"Values of Cloned Objects: " + (r.x) +" " + (r.y));
+	 print.println("Values of Cloned Objects: " + (r.x) +" " + (r.y));
  }
 }
